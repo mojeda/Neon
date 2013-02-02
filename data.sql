6b13bf8195570c3f0bc0b5daa57a152500cffc22
BlueVM <admin@bluevm.com>
2013-02-01 20:23:15 -1000

    Changes to how users login. Removed PHP login as user login will be used instead.

--- data.sql
+++ data.sql
@@ -1,36 +1,8 @@
--- phpMyAdmin SQL Dump
--- version 3.5.1
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Sep 22, 2012 at 08:13 AM
--- Server version: 5.5.24-log
--- PHP Version: 5.3.13
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `data`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `accounts`
---
 
 CREATE TABLE IF NOT EXISTS `accounts` (
   `id` int(8) NOT NULL AUTO_INCREMENT,
   `username` varchar(65) NOT NULL,
-  `password` varchar(65) NOT NULL,
-  `salt` varchar(65) NOT NULL,
   `email` varchar(65) NOT NULL,
   `active` int(1) NOT NULL,
   `activation_code` varchar(65) NOT NULL,
@@ -38,8 +10,55 @@ CREATE TABLE IF NOT EXISTS `accounts` (
   `initial_setup` int(1) NOT NULL DEFAULT '0',
   `stats_email` int(1) NOT NULL,
   `welcome_closed` int(1) NOT NULL,
+  `max_list_files` int(8) NOT NULL DEFAULT '-1',
+  `save_sort_files` int(2) NOT NULL DEFAULT '0',
+  `default_editor` varchar(65) NOT NULL DEFAULT 'default',
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=19 ;
+
+--
+-- Dumping data for table `accounts`
+--
+
+INSERT INTO `accounts` (`id`, `username`, `email`, `active`, `activation_code`, `plan`, `initial_setup`, `stats_email`, `welcome_closed`, `max_list_files`, `save_sort_files`, `default_editor`) VALUES
+(4, 'root', 'admin@localhost', 1, '', 1, 0, 0, 0, 0, 0, 'default');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `domains`
+--
+
+CREATE TABLE IF NOT EXISTS `domains` (
+  `id` int(8) NOT NULL AUTO_INCREMENT,
+  `user_id` int(8) NOT NULL,
+  `domain_name` varchar(65) NOT NULL,
+  `active` int(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=42 ;
+
+--
+-- Dumping data for table `domains`
+--
+
+INSERT INTO `domains` (`id`, `user_id`, `domain_name`, `active`) VALUES
+(41, 18, 'localhost', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `plans`
+--
+
+CREATE TABLE IF NOT EXISTS `plans` (
+  `id` int(8) NOT NULL AUTO_INCREMENT,
+  `name` varchar(65) NOT NULL,
+  `max_domains` int(8) NOT NULL,
+  `max_parked_domains` int(8) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `settings`
@@ -51,7 +70,7 @@ CREATE TABLE IF NOT EXISTS `settings` (
   `setting_value` varchar(65) NOT NULL,
   `setting_group` varchar(65) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
 
 --
 -- Dumping data for table `settings`
@@ -63,7 +82,7 @@ INSERT INTO `settings` (`id`, `setting_name`, `setting_value`, `setting_group`) 
 (3, 'registration_enabled', 'enabled', 'panel_settings'),
 (4, 'forgotpassword_enabled', 'enabled', 'panel_settings'),
 (5, 'default_ip', 'localhost', 'panel_settings'),
-(6, 'max_panel_upload_size', '25M', 'panel_settings');
+(6, 'max_panel_upload_size', '25MB', 'panel_settings');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
--- var/www/neonpanel/includes/functions/users.inc.php
+++ var/www/neonpanel/includes/functions/users.inc.php
@@ -11,8 +11,6 @@ class User extends CPHPDatabaseRecordClass {
 	public $prototype = array(
 		'string' => array(
 			'Username' 	    => "username",
-			'Hash'	            => "password",
-			'Salt'			=>	"salt",
 			'EmailAddress'	    => "email",
 			'ActivationCode'    => "activation_code",
 			'Plan'		=>	"plan",
@@ -29,161 +27,22 @@ class User extends CPHPDatabaseRecordClass {
 		)
 	);
 	
-	public function GenerateSalt(){
-		$this->uSalt = random_string(10);
-	}
-	
-	public function GenerateHash(){
-		if(!empty($this->uSalt)){
-			if(!empty($this->uPassword)){
-				$this->uHash = $this->CreateHash($this->uPassword);
-			} else {
-				throw new MissingDataException("User object is missing a password.");
-			}
-		} else {
-			throw new MissingDataException("User object is missing a salt.");
-		}
-	}
-	
-	public function CreateHash($input){
-		global $cphp_config;
-		$hash = crypt($input, "$5\$rounds=50000\${$this->uSalt}{$cphp_config->settings->salt}$");
-		$parts = explode("$", $hash);
-		return $parts[4];
-	}
-	
-	public function VerifyPassword($password){
-		if($this->CreateHash($password) == $this->sHash){
-			return true;
-		} else {
-			return false;
-		}
-	}
-	
-	public static function ValidateUsername($uUsername){
-		global $database;
-		if($result = $database->CachedQuery("SELECT COUNT(*) FROM accounts WHERE `username` = :Username", array(':Username' => $uUsername), 1)){
-			if($result->data[0]['COUNT(*)'] > 0){
-	               		return false;
-			} else {
-				return true;
-			}
-		}
-	}
-	
-	public static function ValidatePasswords($uPasswordOne, $uPasswordTwo){
-		if($uPasswordOne == $uPasswordTwo){
-			if(strlen($uPasswordOne) > 4){
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	public static function ValidateEmail($uEmailAddress){
-		global $database;
-		if(filter_var($uEmailAddress, FILTER_VALIDATE_EMAIL)) {
-			if($result = $database->CachedQuery("SELECT COUNT(*) FROM accounts WHERE `email` = :Email", array(':Email' => $uEmailAddress), 1)){
-				if($result->data[0]['COUNT(*)'] > 0){
-	               			return false;
-				} else {
-					return true;
-				}
-			}
-		} else {
-			return false;
-		}
-	}
-	
-	public function GenerateAuthorizationCode(){
-		$this->uActivationCode = random_string(25);
-	}
-	
-	public function SendActivationEmail($uEmailAddress){
-	$sPanelTitle = Core::GetSetting('panel_title');
-		$uEmailSubject = $sPanelTitle->sValue." Activation Email";
-		$uEmailContent = '<div align="center">
-			BytePlan Activation<br><hr>
-			<a href="http://byteplan.com/activate.php?id='.$this->uActivationCode.'" target="_blank">Click Here To Activate Your Account</a>
-			</div>';
-		$uEmailHeaders  = "From: noreply@byteplan.com\r\n";
-		$uEmailHeaders .= "Content-type: text/html\r\n";
-		return true; // This is temporary for testing until I have a way to send mail.
-		if (mail($uEmailAddress, $uEmailSubject, $uEmailContent, $uEmailHeaders)) {
-			return true;
-		} else {
-			return false;
-		}
-	}
-	
-	public static function register($uUsername, $uPasswordOne, $uPasswordTwo, $uEmailAddress){
-		global $database;
-		global $root_ssh;
-		global $root_key;
-		global $sWriteLog;
-		if(User::ValidateUsername($uUsername) === true){
-			if(User::ValidatePasswords($uPasswordOne, $uPasswordTwo) === true){
-				if(User::ValidateEmail($uEmailAddress) === true){
-					$sUser = new User(0);
-					
-					$sUser->GenerateAuthorizationCode();
-					if($sUser->SendActivationEmail($uEmailAddress) === true){
-						$sUser->uUsername = $uUsername;
-						$sUser->uPassword = $uPasswordOne;
-						$sUser->GenerateSalt();
-						$sUser->GenerateHash();
-						$sUser->uActive = true;
-						$sUser->uEmailAddress = $uEmailAddress;
-						$sUser->InsertIntoDatabase();
-						$uPassword = stripslashes(str_replace("'", '', $uPasswordOne));
-						if (!$root_ssh->login('root', $root_key)) {
-							fwrite($sWriteLog, 'Login to root via key failed -> System Failed To Connect To The Server - Error #00001'.PHP_EOL);
-							exit('System Failed To Connect To The Server - Error: #00001');
-						}
-						$dev_null = $root_ssh->exec('useradd '.$uUsername);
-						fwrite($sWriteLog, 'useradd '.$uUsername.' -> '.$dev_null.PHP_EOL);
-						$dev_null = $root_ssh->exec('echo -e "'.$uPassword.'\n'.$uPassword.'" | passwd '.$uUsername);
-						fwrite($sWriteLog, 'echo -e "####\n####" | passwd '.$uUsername.' -> '.$dev_null.PHP_EOL);
-						header("Location: register.php?id=activate");
-						die();
-					} else {
-						return "An error occured while attempting to send you an activation email. Please contact us at admin@byteplan.com";
-					}
-				} else {
-					return "The email address you entered was either in use or invalid!";
-				}
-			} else {
-				return "Your passwords must match and must be at least 5 characters in length.";
-			}
-		} else {
-			return "The username you entered is already in use, please try a different username.";
-		} 
-		
-	}
-	
 	public static function login($uUsername, $uPassword){
 		global $database;
 		global $user_ssh;
 		if($result = $database->CachedQuery("SELECT * FROM accounts WHERE (`email` = :Username || `username` = :Username) && `active` = :Active", array(
 		':Username' => $uUsername, ':Active' => '1'), 5)){
 			$sUser = new User($result);
-			if($sUser->VerifyPassword($uPassword)){
 				$_SESSION['user_id'] = $sUser->sId;
 				$uPassword = stripslashes(str_replace("'", '', $uPassword));
 				$_SESSION['password'] = $uPassword;
 				if (!$user_ssh->login($sUser->sUsername, $uPassword)) {
-					fwrite($sWriteLog, 'Login to '.$sUser->sUsername.' via users password failed -> System Failed To Connect To The Server - Error #00002'.PHP_EOL);
-					exit('System Failed To Connect The User To The Server - Error: #00002');
+					return "Invalid Login";
 				}
 				header("Location: main.php");
 				die();
-			} else {
-				return "Invalid password.";
-			}
-		}
-		else
-		{
-			return "Invalid username.";
+		} else {
+			return "Invalid Login.";
 		}
 	}
 }
