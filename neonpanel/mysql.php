a6d51b9c6781d8c065f95fe5371ff792331087e8
BlueVM <admin@bluevm.com>
2013-03-25 16:01:27 -1000

    Fixes for mysql.php

--- neonpanel/mysql.php
+++ neonpanel/mysql.php
@@ -22,6 +22,7 @@ if($LoggedIn === false){
 	
 	if($sAction == create_database){
 		$sDatabaseName = preg_replace("/[^a-z0-9.]+/i", "", $_GET['name']);
+		$sDatabaseName = $sUser->sUsername."_".$sDatabaseName;
 		$sCreateDatabase = $database->CachedQuery("CREATE DATABASE ':Database'", array(':Database' => $sDatabaseName), 1);
 	}
 	
